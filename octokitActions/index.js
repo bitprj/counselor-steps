403ad6fdf3ba0d8b8faab8855e7939917b758b15
Ganning Xu <ganningxu@gmail.com>
2021-08-06 23:08:47 -0400

    add state

--- octokitActions/index.js
+++ octokitActions/index.js
@@ -1,11 +1,15 @@
 const { Octokit } = require("@octokit/core");
+const { OAuthApp, createNodeMiddleware } = require("@octokit/oauth-app");
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const code = req.headers.code;
+
     const state = req.headers.state;
 
+    context.log(code)
+    context.log(state)
     const app = new OAuthApp({
         clientType: "oauth-app",
         clientId: process.env.CLIENT_ID,
@@ -13,13 +17,14 @@ module.exports = async function (context, req) {
         defaultScopes: "repo"
     });
 
-    const { octokit } = await app.getUserOctokit({ code: code });
+    const { octokit } = await app.getUserOctokit({ code, state });
 
+    context.log(octokit);
 
     await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
-        owner,
-        repo,
-        issue_number: "1",
+        owner: "ganning127",
+        repo: "asdfdas",
+        issue_number: 1,
         body: 'created via serverless'
     })
 
