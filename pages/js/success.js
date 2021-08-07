9a3289455ba00d0b8dc903a0c2055a040bb1d2e3
Ganning Xu <ganningxu@gmail.com>
2021-08-06 23:11:47 -0400

    add state

--- pages/js/success.js
+++ pages/js/success.js
@@ -6,13 +6,16 @@ window.onload = async () => {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const code = urlParams.get('code');
+    const state = urlParams.get('state');
+
+    console.log(state);
 
     console.log(code);
 
     const endpoint = "https://hasuraprogressupdate.azurewebsites.net/api/octokitActions?code=yNZKwqGeRcuvFCUIm4MaS0u/doKx0z0y/gwYyQXbM5x3OSUGVysaOw==";
     const options = {
         headers: {
-            code: code
+            code, state
         }
     }
     let resp = await fetch(endpoint, options);
